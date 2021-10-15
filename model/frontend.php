8b270b916c9f915bb039dff29fad409e75f24a91
Léo Moille <leomoille18@gmail.com>
2021-10-15 18:25:54 +0200

    Add twig for templating

--- model/frontend.php
+++ model/frontend.php
@@ -1,9 +1,23 @@
 <?php
 
+function dbConnect()
+{
+    try {
+        $db = new PDO('mysql:host=localhost;dbname=leomoilledotcom;charset=utf8', 'root', '');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        
+        return $db;
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
 function getPosts()
 {
-    $db  = dbConnect();
-    $req = $db->query(
+    $db = dbConnect();
+    
+    return $db->query(
         '
     SELECT id,
            author,
@@ -12,11 +26,27 @@ function getPosts()
            content,
            DATE_FORMAT(publication_date, \'%d/%m/%Y à %Hh%imin\') AS publication_date,
            DATE_FORMAT(modification_date, \'%d/%m/%Y à %Hh%imin\') AS modification_date
-    FROM articles ORDER BY publication_date DESC LIMIT 0, 5
+    FROM articles ORDER BY publication_date
     '
     );
+}
+
+function getLastPosts()
+{
+    $db = dbConnect();
     
-    return $req;
+    return $db->query(
+        '
+    SELECT id,
+           author,
+           title,
+           pre_content,
+           content,
+           DATE_FORMAT(publication_date, \'%d/%m/%Y à %Hh%imin\') AS publication_date,
+           DATE_FORMAT(modification_date, \'%d/%m/%Y à %Hh%imin\') AS modification_date
+    FROM articles ORDER BY publication_date DESC LIMIT 0, 3
+    '
+    );
 }
 
 function getPost($postId)
@@ -35,35 +65,18 @@ function getPost($postId)
     '
     );
     $req->execute(array($postId));
-    $post = $req->fetch();
     
-    return $post;
+    return $req->fetch();
 }
 
 function getComments($postId)
 {
     $db       = dbConnect();
     $comments = $db->prepare(
-        '
-    SELECT id,
-           author,
-           comment,
-           DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\') AS comment_date
-    FROM comments WHERE post_id = ? ORDER BY comment_date DESC'
+        'SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin\')
+                AS comment_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC'
     );
     $comments->execute(array($postId));
     
-    return $comments;
-}
-
-// Nouvelle fonction qui nous permet d'éviter de répéter du code
-function dbConnect()
-{
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=leomoilledotcom;charset=utf8', 'root', '');
-        
-        return $db;
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+    return $comments->fetchAll();
 }
